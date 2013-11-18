c568a52633cdaef98387e82522b1d9f21d108f5a
sseffa <karagozsefa@gmail.com>
2013-11-18 14:41:07 +0200

    bug fixed articles view

--- app/views/frontend/tag/index.blade.php
+++ app/views/frontend/tag/index.blade.php
@@ -12,27 +12,32 @@
             @yield('partial/breadcrumbs', Breadcrumbs::render('blog', $articles))
         </div>
     </div>
-    @foreach( $articles as $article )
-    <div class="row">
-      <div class="col-lg-8">
-            <h3><a href="#">{{ $article->title }}</a></h3>
-            <p>{{  date("d M Y a\t H:i a",strtotime($article->created_at)) }}</p><p>by <a href="#">Sefa</a></p>
-            <p>{{{ mb_substr(strip_tags($article->content),0,600) }}}</p>
-             <a href="{{ URL::route('dashboard.article.show', array('id'=>$article->id, 'slug'=>$article->slug)) }}" class="btn btn-primary">Read More</a>
-        </div>
-       <h4>Tags</h4>
-       <div class="col-lg-4">
-          <div class="row">
-                <div class="tagcloud tabbed_tag">
-                    @foreach($tags as $tag)
-                      <a href="{{ URL::route('dashboard.tag', array('tag'=>$tag->name)) }}">{{ $tag->name }}</a>
-                    @endforeach
+
+        <div class="row">
+             <div class="col-lg-8">
+             @if($articles->count())
+             @foreach( $articles as $article )
+                <h3><a href="#">{{ $article->title }}</a></h3>
+                <p>{{  date("d M Y a\t H:i a",strtotime($article->created_at)) }}</p><p>by <a href="#">Sefa</a></p>
+                <p>{{{ mb_substr(strip_tags($article->content),0,600) }}}</p>
+                <a href="{{ URL::route('dashboard.article.show', array('id'=>$article->id, 'slug'=>$article->slug)) }}" class="btn btn-primary">Read More</a>
+            @endforeach
+            @else
+            <div class="alert alert-danger">No results found</div>
+            @endif
+           </div>
+           <h4>Tags</h4>
+           <div class="col-lg-4">
+              <div class="row">
+                    <div class="tagcloud tabbed_tag">
+                        @foreach($tags as $tag)
+                          <a href="{{ URL::route('dashboard.tag', array('tag'=>$tag->name)) }}">{{ $tag->name }}</a>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <hr>
-    @endforeach
+        <hr>
     <div class="pull-left">
         <ul class="pagination">
             {{ $articles->links() }}
