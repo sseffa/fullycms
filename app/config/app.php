8d275b38ac2e8a906583384f61dfea342dc38668
Sefa Karagöz <karagozsefa@gmail.com>
2014-10-06 20:25:32 +0300

    delete way generators package

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,6 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Way\Generators\GeneratorsServiceProvider',
 		'Cartalyst\Sentry\SentryServiceProvider',
 		'Intervention\Image\ImageServiceProvider',
 		'DaveJamesMiller\Breadcrumbs\ServiceProvider',
