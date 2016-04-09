a664b2d4ad006941f66610fecdf1b3cdb7322c54
rengang <grug@foxmail.com>
2016-04-09 13:25:15 +0800

    Add zh support

---
+++ resources/lang/zh/fully.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+    /* footer start */
+    'newsletter'          => '联系我们',
+    'button_save'         => '确定',
+    'enter_your_email'    => '请输入你的邮箱',
+    'all_rights_reserved' => 'All Rights Reserved',
+    /* footer end */
+    /* contact page start */
+    'contact_form'        => '联系我们',
+    'our_location'        => '联系地址',
+    'name_and_surname'    => '姓名',
+    'email'               => '邮箱地址',
+    'phone_number'        => '电话',
+    'subject'             => '主题',
+    'message'             => '详细信息',
+    'button_send'         => '发送',
+    /* contact page end */
+
+    /*pagination start*/
+    'link_previous'       => '上一页',
+    'link_next'           => '下一页',/*pagination end*/);
